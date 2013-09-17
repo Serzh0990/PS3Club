5fab19ce424b2b76640e27b0aeddebccdbb11f82
Admin <Admin@Admins-Mac.local>
2013-09-17 20:48:16 +0300

    PS3CLub OSX initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MyAppV40::Application.routes.draw do
+  get "pages/about"
+  get "pages/contacts"
   resources :news
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
