f2119096814e08255dfe0ec9ce7aad74a6cd9f08
Admin <Admin@Admins-Mac.local>
2013-09-26 01:06:24 +0300

    ayaebyzaebalsa

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -1,13 +1,15 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.root = Rails.root.join('tmp')
-    config.cache_dir = 'carrierwave'
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
 
-    config.storage = :s3
-    config.s3_access_key_id = ENV['AKIAIUKYFSMSHJDCZP4Q']
-    config.s3_secret_access_key = ENV['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']
-    config.s3_bucket = ENV['images']
-  else
-    config.storage = :file
-  end
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'AKIAIUKYFSMSHJDCZP4Q',                        # required
+    :aws_secret_access_key  => '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',                     # required
+    :region                 => 'Singapore',                  # optional, defaults to 'us-east-1'
+  
+  }
+  config.fog_directory  = 'images'                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
