d0ef1fa89c5d22f1b22aa5a9ab3cb57f88bfaa24
Admin <Admin@Admins-Mac.local>
2013-09-26 01:18:33 +0300

    aswmvbd

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,12 +14,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   
   def store_dir
-       "ps3club/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
    end
-   
-   def cache_dir
-       "#{Rails.root}/tmp/uploads"
-     end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
