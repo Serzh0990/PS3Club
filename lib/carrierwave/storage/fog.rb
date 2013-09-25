39af013cb63e44e121d195e7011931dc5c78d7f1
Admin <Admin@Admins-Mac.local>
2013-09-26 00:54:29 +0300

    asd

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -1,22 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AKIAIUKYFSMSHJDCZP4Q'],                        # required
-    :aws_secret_access_key  => ENV['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'],                        # required
-    #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    #:host                   => 's3.example.com',             # optional, defaults to nil
-    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
+  if Rails.env.production?
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
+
+    config.storage = :s3
+    config.s3_access_key_id = ENV['AKIAIUKYFSMSHJDCZP4Q']
+    config.s3_secret_access_key = ENV['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']
+    config.s3_bucket = ENV['images']
   else
-    config.storage = :fog
+    config.storage = :file
   end
-  config.cache_dir = "#{Rails.root}/tmp/ps3club"
-  config.fog_directory  = ENV['images']                    # required
-  config.fog_public = false                            # optional, defaults to true
- # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
